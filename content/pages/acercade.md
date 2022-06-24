c356a0f719fee5085c62529cb5f551ec02112d96
rodrigo <rodrigo@localhost.localdomain>
2022-06-24 00:23:45 -0400

    acercade y google analytics

---
+++ content/pages/acercade.md
@@ -0,0 +1,10 @@
+Title: Acerca de
+Date: 2022-06-23 23:40
+Category: Bio
+Tags: pelican, publishing
+Slug: mi-bio
+Author: Rodrigo Lara
+Summary: Biografía
+Lang: es
+
+Rodrigo Lara nació en Santiago el 15 de junio de 1989 y estudió ingeniería civil en electricidad en la Universidad de Santiago de Chile
--- publishconf.py
+++ publishconf.py
@@ -7,7 +7,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = ''
+SITEURL = 'https://rodrigolara-cl.github.io'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
@@ -18,4 +18,4 @@ DELETE_OUTPUT_DIRECTORY = True
 # Following items are often useful when publishing
 
 #DISQUS_SITENAME = ""
-#GOOGLE_ANALYTICS = ""
+GOOGLE_ANALYTICS = "G-S9G4CVQBN6"
